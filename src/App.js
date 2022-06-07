4a69bf0538587c3ffe5f8fdcca8db7a18e2af84d
ChienNM <chiennm@ati.vn>
2022-06-07 11:12:53 +0700

    Add native action

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Button, Input, Col, Row } from 'antd';
+import './App.scss';
+
+const { TextArea } = Input;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [type, setType] = useState('');
+    const [data, setData] = useState('');
+
+    const handleReset = () => {
+        setType('');
+        setData('');
+    };
+
+    const handleTest = () => {
+        const actionData = {
+            type,
+            data: JSON.parse(data),
+        };
+        window?.ReactNativeWebView?.postMessage(JSON.stringify(actionData));
+    };
+
+    return (
+        <div className='container'>
+            <Row>
+                <Col span={24}>
+                    <Input
+                        addonBefore='TYPE'
+                        size='large'
+                        placeholder='Ví dụ: GO_BACK'
+                        value={type}
+                        onChange={(e) => setType(e.target.value)}
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col span={24}>
+                    <TextArea
+                        rows={8}
+                        size='large'
+                        placeholder='Data - Ví dụ: { "tel": "HOTLINE" }'
+                        value={data}
+                        onChange={(e) => setData(e.target.value)}
+                    />
+                </Col>
+            </Row>
+            <Row gutter={10}>
+                <Col span={12}>
+                    <Button
+                        type='primary'
+                        size='large'
+                        style={{ width: '100%' }}
+                        onClick={handleTest}
+                    >
+                        Test
+                    </Button>
+                </Col>
+                <Col span={12}>
+                    <Button size='large' style={{ width: '100%' }} onClick={handleReset}>
+                        Reset
+                    </Button>
+                </Col>
+            </Row>
+        </div>
+    );
 }
 
 export default App;
